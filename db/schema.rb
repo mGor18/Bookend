2b5997b8a80b1913edb99a31b3986d038684502d
Matt Gordon <mattg1520@gmail.com>
2020-07-01 17:00:00 +0300

    db migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_135801) do
+ActiveRecord::Schema.define(version: 2020_07_01_135918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 2020_07_01_135801) do
     t.index ["user_id"], name: "index_borrows_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "borrow_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["borrow_id"], name: "index_messages_on_borrow_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,4 +60,6 @@ ActiveRecord::Schema.define(version: 2020_07_01_135801) do
 
   add_foreign_key "borrows", "books"
   add_foreign_key "borrows", "users"
+  add_foreign_key "messages", "borrows"
+  add_foreign_key "messages", "users"
 end
