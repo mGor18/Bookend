3ccf7223718d3e418eaafa703dcd64b4c752c84d
Matt Gordon <mattg1520@gmail.com>
2020-07-01 16:58:43 +0300

    db migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_135001) do
+ActiveRecord::Schema.define(version: 2020_07_01_135801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2020_07_01_135001) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "borrows", force: :cascade do |t|
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_borrows_on_book_id"
+    t.index ["user_id"], name: "index_borrows_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,4 +48,6 @@ ActiveRecord::Schema.define(version: 2020_07_01_135001) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "borrows", "books"
+  add_foreign_key "borrows", "users"
 end
