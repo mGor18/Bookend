0dfd397827396b94459cf7de565c1b90630f11f0
Matt Gordon <mattg1520@gmail.com>
2020-07-07 00:43:18 +0300

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,18 @@ require 'faker'
 10.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
+  email = Faker::Internet.email
+  password = '123456'
 
+  new_user = User.new(email: email, password: password, first_name: first_name, last_name: last_name)
+
+  5.times do
+    title = Faker::Book.title
+    author = Faker::Book.author
+    synopsis = Faker::Lorem.sentences(word_count: rand(1..5))
+
+    new_book = Book.new(title: title, author: author, isPrivate: false, synopsis: synopsis, book_status: free)
+    new_book.save!
+  end
+  new_user.save!
 end
