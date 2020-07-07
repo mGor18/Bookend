74fef16522b62514b09d4052a700db4e97ceb5ee
Matt Gordon <mattg1520@gmail.com>
2020-07-07 18:30:22 +0300

    added user foreign_key to books column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_214406) do
+ActiveRecord::Schema.define(version: 2020_07_07_145402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2020_07_06_214406) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "book_status"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "borrows", force: :cascade do |t|
@@ -81,6 +83,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_214406) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "books", "users"
   add_foreign_key "borrows", "books"
   add_foreign_key "borrows", "users"
   add_foreign_key "messages", "borrows"
